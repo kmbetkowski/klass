4b2b0cf3938d92c5621386a67fe89236b4ec486e
Kamil Betkowski <specializeed1@gmail.com>
2024-02-03 20:03:47 +0100

    nowe zadania

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,3 @@ class przestawTablice {
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,4,3,2,1};
-        int n=4;
-
-        int[] wynik = przestawTablice.przestawTablice(nums, n);
-
-        for(int i=0; i<wynik.length; i++) {
-            System.out.print(wynik[i]);
-            if(i < wynik.length - 1) {
-                System.out.print(", ");
-            }
-        }
-
-    }
-}
